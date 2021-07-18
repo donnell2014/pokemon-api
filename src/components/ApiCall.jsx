a0cca9bc73421fb1925f178cd46ad1cca38452e5
Donnell Albertie <dalbertie5@gmail.com>
2021-07-18 14:19:04 -0500

    pokemonApi

--- src/components/ApiCall.jsx
+++ src/components/ApiCall.jsx
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from "react";
-
+import axios from "axios";
 const ApiCall = props => {
-    const [pokemon, setPokemon] = useState(null);
+    const [pokemon, setPokemon] = useState([]);
     useEffect(() => {
-        fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
-        .then(res => {
-            return res.json()
-        })
+        // fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
+        // .then(res => {
+        //     return res.json()
+        // })
+        // .then(res => {
+        //     console.log(res)
+        //     setPokemon(res.results)
+        // })
+        // .catch(err => {
+        //     console.log(err)
+        // })
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
         .then(res => {
             console.log(res)
-            setPokemon(res.results)
+            setPokemon(res.data.results)
         })
         .catch(err => {
             console.log(err)
@@ -18,6 +26,10 @@ const ApiCall = props => {
     return(
         <div>
             <h1> Api component.</h1>
+            <button onClick={(pokemon.map(poke => {
+                    return <h3>{poke.name}</h3>
+                }))
+            }>Fetch Pokemon</button>
             {/* <h3>Name: {pokemon.name}</h3> */}
             {/* <img src={pokemon.sprites.front_default} alt={pokemon.name} /> */}
             {
